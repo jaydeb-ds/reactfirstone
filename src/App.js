d4e0605da8fa01149023741547b4ebfb19f38fec
jaydeb-ds <das294761@gmail.com>
2025-01-12 23:36:59 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -12,48 +12,7 @@ let obj = {
   "college name":"BTCST"
 }
   return(
-    <div>
-    <h1 className="main">Tripura University</h1>
-    <div className="main2">
-      <h2>College Name = {obj['college name']}</h2>
-      <h2>course = <span>{obj['course']}</span></h2>
-      <h3>Student name = {name}</h3>
-
-      <table className="main">
-        <caption>marksheet</caption>
-        <thead>
-          <tr>
-            <th>subject</th>
-            <th>Mark</th>
-          </tr>
-        </thead>
-        <tfoot>
-            <tr>
-              <th>total</th>
-              <th>888</th>
-            </tr>
-          </tfoot>
-          <tbody>
-            <tr>
-              {
-                value = ()=>{
-                  let i=0;
-               while (i<2) {
-                console.log(mark[i])
-                return mark[i]
-                i++;
-               }
-              } 
-
-              }
-            
-            </tr>
-          </tbody>
-      </table>
-
-    </div>
-    
-    </div>
+    <h1>hellow</h1>
   )
 }
 
